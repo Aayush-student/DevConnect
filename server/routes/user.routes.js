06c3d1b16dde33e97ea0efe0458105aa3f0ffda2
Aayush-student <Aayush07dz@gmail.com>
2026-03-19 05:32:39 +0530

    temp rename middleware folder

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { registerUser,loginUser,logoutUser,getUserData } from "../controllers/users.controller.js";
-import { verifyjwt } from "../middlewares/auth.middleware.js";
+import { verifyjwt } from "../temp/auth.middleware.js";
 
 const userRouter = Router();
 
